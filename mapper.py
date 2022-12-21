6701a8d8ea0b01ca99473af8a41f290637025250
YatharthPandey01 <119107646+YatharthPandey01@users.noreply.github.com>
2022-12-21 19:37:03 +0530

    Add files via upload
    Hi , this includes mapper and reducer files and a hadoop library which are essential to execute a map-reduce problem in hadoop.

---
+++ mapper.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+  
+# import sys because we need to read and write data to STDIN and STDOUT
+import sys
+  
+# reading entire line from STDIN (standard input)
+for line in sys.stdin:
+    # to remove leading and trailing whitespace
+    line = line.strip()
+    # split the line into words
+    #line="Hadoop is old now"
+    words = line.split()
+    #words=["Hadoop","is","old","now"]
+      
+    # we are looping over the words array and printing the word
+    # with the count of 1 to the STDOUT
+    for word in words:
+        # write the results to STDOUT (standard output);
+        # what we output here will be the input for the
+        # Reduce step, i.e. the input for reducer.py
+        print('%s\t%s' % (word, 1))
+        #Hadoop 1
+        #is 1
+        #old 1
+        #now 1
---
+++ reducer.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+  
+from operator import itemgetter
+import sys
+  
+current_word = None
+current_count = 0
+word = None
+
+#car 1
+#car 2
+#car 5 
+#car 6
+#mat 1
+#bat 1
+#cat 2   # this is what the reducer will read , in the form of lines 
+# read the entire line from STDIN
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+    #line="car 1"
+    # slpiting the data on the basis of tab we have provided in mapper.py
+    word, count = line.split('\t', 1)
+    #word="car"
+    #count="1"
+    # convert count (currently a string) to int
+    try:
+        count = int(count)
+        #count=1 it will become an int
+    except ValueError:
+        # count was not a number, so silently
+        # ignore/discard this line
+        continue
+  
+    # this IF-switch only works because Hadoop sorts map output
+    # by key (here: word) before it is passed to the reducer
+    if current_word == word:
+        current_count += count
+    else:
+        if current_word:
+            # write result to STDOUT
+            print('%s\t%s' % (current_word, current_count))
+        current_count = count
+        current_word = word
+  
+# do not forget to output the last word if needed!
+if current_word == word:
+    print('%s\t%s' % (current_word, current_count))
